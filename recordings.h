ec4800a0bb213c9cce8fe8ef32b0869dbd265af8
Jony424 <122822625+Jony424@users.noreply.github.com>
2023-01-30 02:37:14 +0200

    Add files via upload

---
+++ recordings.h
@@ -0,0 +1,135 @@
+const int8_t Alla_yustor_1 = 0x01;
+const int8_t Hashem_yerachem_1 = 0x02;
+const int8_t Ma_ata_dafok_1 = 0x03;
+const int8_t Yesh_hashash = 0x04;
+const int8_t Azovti_beima_1 = 0x05;
+const int8_t Laas_li_et_harosh_1 = 0x06;
+const int8_t Ale_vehatslach_1 = 0x07;
+const int8_t Gmar_hatima_1 = 0x08;
+const int8_t Kilhalan_1 = 0x09;
+const int8_t Kilhalan_2 = 0x0A;
+const int8_t Kilhalan_3 = 0x0B;
+const int8_t Sheina_areva_1 = 0x0C;
+const int8_t Sheina_areva_2 = 0x0D;
+const int8_t Delax_1 = 0x0E;
+const int8_t Delax_2 = 0x0F;
+const int8_t Delax_3 = 0x10;
+const int8_t Delax_4 = 0x11;
+const int8_t Intergalacty_1 = 0x12;
+const int8_t Intergalacty_2 = 0x13;
+const int8_t Nae_1 = 0x14;
+const int8_t Nae_2 = 0x15;
+const int8_t Nae_3 = 0x16;
+const int8_t Nimkar_1 = 0x17;
+const int8_t Ptsatsa_1 = 0x18;
+const int8_t Sasgoni_1 = 0x19;
+const int8_t Tzukar_1 = 0x1A;
+const int8_t Bo_nivrach_1 = 0x1B;
+const int8_t Livzoz_Leaktzetz_1 = 0x1C;
+const int8_t Angry_bird = 0x1D;
+const int8_t bareket = 0x1E;
+const int8_t Gal_gabay_1 = 0x1F;
+const int8_t Gal_gabay_2 = 0x20;
+const int8_t Gal_gabay_3 = 0x21;
+const int8_t physics_girl = 0x22;
+const int8_t Ata_mitnamnem_1 = 0x23;
+const int8_t Enough_scooby_doo_1 = 0x24;
+const int8_t Ma_heeleta_1 = 0x25;
+const int8_t Harotza_et_harav_1 = 0x26;
+const int8_t Holchim_larav_1 = 0x27;
+const int8_t Kol_hayechida_maniakim_1 = 0x28;
+const int8_t Maase_shehaya_1 = 0x29;
+const int8_t Pakid_gavoha_1 = 0x2A;
+const int8_t Habuba_num_1 = 0x2B;
+const int8_t Habuba_num_2 = 0x2C;
+const int8_t Hasatlan_hakatan_1 = 0x2D;
+const int8_t Hod_Batlanutcha_1 = 0x2E;
+const int8_t hod_namnemutha = 0x2F;
+const int8_t Leitzan_1 = 0x30;
+const int8_t potatos_bag = 0x31;
+const int8_t Sus_yeor_1 = 0x32;
+
+const int numOfLibraries = 9;
+const int biggestLibraryLength = 13;
+
+const int8_t libraries[numOfLibraries][biggestLibraryLength] = {
+  { //conspiracies
+    Bo_nivrach_1,
+    Livzoz_Leaktzetz_1,
+  },
+  { //enthusiasm
+    Delax_1,
+    Delax_2,
+    Delax_3,
+    Delax_4,
+    Intergalacty_1,
+    Intergalacty_2,
+    Nae_1,
+    Nae_2,
+    Nae_3,
+    Nimkar_1,
+    Ptsatsa_1,
+    Sasgoni_1,
+    Tzukar_1,
+  },
+  { //expression of horror
+    Alla_yustor_1,
+    Hashem_yerachem_1,
+    Ma_ata_dafok_1,
+    Yesh_hashash,
+  },
+  { //greetings
+    Ale_vehatslach_1,
+    Gmar_hatima_1,
+    Kilhalan_1,
+    Kilhalan_2,
+    Kilhalan_3,
+    Sheina_areva_1,
+    Sheina_areva_2,
+  },
+  { //Impatience
+    Azovti_beima_1,
+    Laas_li_et_harosh_1,
+  },
+  { //nicknames
+    Habuba_num_1,
+    Habuba_num_2,
+    Hasatlan_hakatan_1,
+    Hod_Batlanutcha_1,
+    hod_namnemutha,
+    Leitzan_1,
+    potatos_bag,
+    Sus_yeor_1,
+  },
+  { //others
+    Angry_bird,
+    bareket,
+    Gal_gabay_1,
+    Gal_gabay_2,
+    Gal_gabay_3,
+    physics_girl,
+  },
+  { //urging
+    Ata_mitnamnem_1,
+    Enough_scooby_doo_1,
+    Ma_heeleta_1,
+  },
+  { //wisdom
+    Harotza_et_harav_1,
+    Holchim_larav_1,
+    Kol_hayechida_maniakim_1,
+    Maase_shehaya_1,
+    Pakid_gavoha_1,
+  },
+};
+const int8_t librariesLengths[numOfLibraries] = {
+  2,
+  13,
+  4,
+  7,
+  2,
+  8,
+  6,
+  3,
+  5,
+};
