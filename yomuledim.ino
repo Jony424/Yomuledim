9b8f3c62c9e79fc1d756954f41be4bd7485ab04d
Jony424 <122822625+Jony424@users.noreply.github.com>
2023-01-30 02:36:29 +0200

    Update yomuledim.ino

--- yomuledim.ino
+++ yomuledim.ino
@@ -1,131 +1,104 @@
-#include <NeoSWSerial.h>
+#include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
+#include <avr/sleep.h>
+#include "recordings.h"
 
 
 // Buttons pin definitions:
 #define buttonPin1 4 // the number of the "play random song" button pin (stomach)
-#define buttonPin2 5  // the number of the "move one library backward" button pin (button2)
-#define buttonPin3 6  // the number of the "move one library forward" button pin (button3)
+#define buttonPin2 2  // the number of the "move one library backward" button pin (button2)
+#define buttonPin3 3  // the number of the "move one library forward" button pin (button3)
 
 //Leds pin definitions:
 static const int LED_COUNT = 2;  // Number of LEDs in the strip
-static const int LED_PIN = 7;  // Pin where the LED is connected
+static const int LED_PIN = 9;  // Pin where the LED is connected
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 
 //Mp3 player pin definition
-#define ARDUINO_RX 3 //should connect to TX of the Serial MP3 Player module
-#define ARDUINO_TX 2 //connect to RX of the module
-NeoSWSerial myMP3(ARDUINO_RX, ARDUINO_TX);
+#define ARDUINO_RX 6 //should connect to TX of the Serial MP3 Player module
+#define ARDUINO_TX 5 //connect to RX of the module
+SoftwareSerial myMP3(ARDUINO_RX, ARDUINO_TX);
 
 
 //Variables for reading the pushbutton current and previous state
-int buttonState_1 = 0;  
-int buttonState_2 = 0;  
-int buttonState_3 = 0;  
+int stomach_buttonState = 0;  
+int leftHand_buttonState = 0;  
+int rightHand_buttonState = 0;  
 
-int previousButton_1_State = HIGH;  // for checking the state of a pushButton
-int previousButton_2_State = HIGH;  // for checking the state of a pushButton
-int previousButton_3_State = HIGH;  // for checking the state of a pushButton
+int stomach_previousButton_State = HIGH;  // for checking the state of a pushButton
+int leftHand_previousButton_State = HIGH;  // for checking the state of a pushButton
+int rightHand_previousButton_State = HIGH;  // for checking the state of a pushButton
+
+bool pressed_both_buttons = false;
+bool stomach_button = false;
+bool left_hand_button = false;
+bool right_hand_button = false;
 
 
 
 static int8_t Send_buf[6] = {0} ;
 /************Command byte**************************/
-/*basic commands*/
-#define CMD_PLAY  0X01
-#define CMD_PAUSE 0X02
-#define CMD_NEXT_SONG 0X03
-#define CMD_PREV_SONG 0X04
-#define CMD_VOLUME_UP   0X05
-#define CMD_VOLUME_DOWN 0X06
-#define CMD_FORWARD 0X0A // >>
-#define CMD_REWIND  0X0B // <<
-#define CMD_STOP 0X0E
-#define CMD_STOP_INJECT 0X0F//stop interruptting with a song, just stop the interlude
-
 /*5 bytes commands*/
 #define CMD_SEL_DEV 0X35
-  #define DEV_TF 0X01
-#define CMD_IC_MODE 0X35
-  #define CMD_SLEEP   0X03
-  #define CMD_WAKE_UP 0X02
-  #define CMD_RESET   0X05
+#define DEV_TF 0X01
 
 /*6 bytes commands*/  
 #define CMD_PLAY_W_INDEX   0X41
 #define CMD_PLAY_FILE_NAME 0X42
-#define CMD_INJECT_W_INDEX 0X43
 
-/*Special commands*/
-#define CMD_SET_VOLUME 0X31
 #define CMD_PLAY_W_VOL 0X31
-#define CMD_SET_PLAY_MODE 0X33
-  #define ALL_CYCLE 0X00
-  #define SINGLE_CYCLE 0X01
-#define CMD_PLAY_COMBINE 0X45//can play combination up to 15 songs
-
-uint8_t volume = 0X10;
-uint8_t songId;
-
-//red array
-const int8_t yesh_anashim = 0X03;
-const int8_t san_fransisco = 0X04;
-
-//green array
-const int8_t saa_leat = 0X05;
-const int8_t od_ihiye = 0X06;
-const int8_t od_nipagesh = 0X07;
-
-//blue array
-const int8_t atur_mizheh = 0X01;
-const int8_t yesh_bi_ahava = 0X02;
-
-//Cyan array
-const int8_t oof_gozal = 0X08;
 
 
-//{red_library, green_library, blue_library, yellow_library, cyan_library, magneta_library, white_library};
-/*
-  IMPORTATN!!!
-  When adding/removing songs/library from 'libraries' 2D array make sure:
- - numOfLibraries and biggestLibraryLength values are up to date and correct
- - librariesLengths array is updated with the correct lengths of each library in 'libraries', respectively to the position of each library. e.g: if the libraries[0] library has the length of 3,
-   librariesLengths[0] should be also 3 and will look like this: librariesLengths[numOfLibraries] = {3,x,y, ...}
- - colors array consist a respective color to your new library (if added one)
-*/
-const int numOfLibraries = 4;
-const int biggestLibraryLength = 4;
-const int8_t libraries[numOfLibraries][biggestLibraryLength] = {{yesh_anashim, san_fransisco}, {saa_leat, od_ihiye, od_nipagesh}, {atur_mizheh, yesh_bi_ahava}, {oof_gozal}};
-const int8_t librariesLengths[numOfLibraries] = {2,3,2,1}; //only way to get the length of an 'internal' array in a 2d array, with different lengths. UPDATE IT ACCORDING TO 'libraries' UPON ANY CHANGE!!!
-
-
-// Define an array of colors
-uint32_t colors[] = {
-  strip.Color(255, 0, 0),  // Red
+// Define an array of                     
+uint32_t colors[] = { //add/remove color according to libraries changes
+  strip.Color(255, 0, 0),  // Red 
   strip.Color(0, 255, 0),  // Green
   strip.Color(0, 0, 255),  // Blue
   strip.Color(0, 255, 255),  // Cyan
   strip.Color(255, 255, 0),  // Yellow
   strip.Color(255, 0, 255),  // Magenta
-  strip.Color(255, 255, 255)  // White
+  strip.Color(255, 255, 255),  // White
+  strip.Color(255, 165, 0),  // Orange
+  strip.Color(255, 192, 203)  // Pink
 };
+
+//uint8_t volume = 0X20;
+uint8_t volume = 0X20;
+uint8_t songId;
+
+int mainDelay = 100; //Delay time between each loop of the main func (in milli seconds)
 
 int libraryLength;
 int currentLibraryIndex = 0; //set red library as default
+int previousLibraryIndex = 0;
+
+int brightness = 255;
+int dimming_time = 5; //how much time (sec) will it take to dim the led completly off
+unsigned long startTime;
+const long interval = 5000;
 
 int8_t randomInt = -1;
+
+int timeWindow = 70;
 
 void sendCommand(int8_t command, int16_t dat );
 
 void setup() {
+  //turn off builtin led to save power
+  pinMode(LED_BUILTIN,OUTPUT);//We use the led on pin 13 to indecate when Arduino is A sleep
+  digitalWrite(LED_BUILTIN,LOW);//turning LED off
+
   // initialize the pushbutton pin as an input:
+  startTime = millis();
   pinMode(buttonPin1, INPUT_PULLUP);
   pinMode(buttonPin2, INPUT_PULLUP);
   pinMode(buttonPin3, INPUT_PULLUP);
 
   // Initialize all led pixels to 'off'
   strip.begin();
+  strip.setPixelColor(0, colors[currentLibraryIndex]);  // Set the first LED (eye) to the current color in the array
+  strip.setPixelColor(1, colors[currentLibraryIndex]);  // Set the second LED (eye) to the current color in the array
   strip.show();  
 
   // mp3 chip initialization
@@ -133,7 +106,6 @@ void setup() {
 	delay(500);//Wait chip initialization is complete
   sendCommand(CMD_SEL_DEV, DEV_TF);//select the TF card  
 	delay(500);//wait for 200ms
-  // mp3 chip initialization
   
   Serial.begin(9600);
   randomSeed(analogRead(A0)); 
@@ -141,55 +113,145 @@ void setup() {
 
 void loop() {
   // read the state of the pushbutton value:
-  buttonState_1 = digitalRead(buttonPin1);
-  buttonState_2 = digitalRead(buttonPin2);
-  buttonState_3 = digitalRead(buttonPin3);
+  stomach_buttonState = digitalRead(buttonPin1);
+  leftHand_buttonState = digitalRead(buttonPin2);
+  rightHand_buttonState = digitalRead(buttonPin3);
+  
+  pressed_both_buttons = false;
+  stomach_button = (stomach_buttonState != stomach_previousButton_State) && stomach_buttonState == LOW;  
+  left_hand_button = (leftHand_buttonState != leftHand_previousButton_State) && leftHand_buttonState == LOW;  
+  right_hand_button = (rightHand_buttonState != rightHand_previousButton_State) && rightHand_buttonState == LOW;  
 
+
+  if ((left_hand_button) || (right_hand_button)){
+    Serial.println("Pressed one of the buttons");    
+    if (left_hand_button){
+      //wait for button 3 for 50 ms
+      pressed_both_buttons = wait_for_button_press(buttonPin3, timeWindow);
+      }
+    else if (right_hand_button){
+      //wait for button 2 for 50 ms (pin= ,time window)
+      pressed_both_buttons = wait_for_button_press(buttonPin2, timeWindow);
+    }
+  }
+
+  if (pressed_both_buttons){
+    rainbow(10);      
+  }
   // Button 1 case: play a random song out ouf the current library
-  if ((buttonState_1 != previousButton_1_State) && buttonState_1 == LOW){
-    playRandomSong(currentLibraryIndex);
-    delay(50);
+  else if (stomach_button){
+    clicked_stomach();
+  }
+  else if (left_hand_button){
+    clicked_left_hand();  // Left hand case: move one library 'backward' and play a random song out of it
+  }
+  else if (right_hand_button){
+    clicked_right_hand(); // Right hand case: move one library 'forward' and play a random song out of it
+  }
+
+  if (currentLibraryIndex != previousLibraryIndex){ //if changed library
+    turn_on_leds();
+    previousLibraryIndex = currentLibraryIndex;
+  }
+  else if (millis() - startTime >= interval) { // 5 seconds have passed
+    if (strip.getBrightness() != 0) {    
+      dimLeds();
+      mainDelay = (dimming_time*1000)/127; // (dimmimng_time*1000)/(255/brightness_interval)
+    }
+    else if (millis() - startTime >= 60*1000) { // Go to sleep after 30 seconds from the last activity
+      Going_To_Sleep();
+      mainDelay = 100; 
+    }
   }
   
-  // Button 2 case: move one library 'backward' and play a random song out of it
-  else if ((buttonState_2 != previousButton_2_State) && buttonState_2 == LOW){
-    if (currentLibraryIndex == 0){
-      currentLibraryIndex = numOfLibraries-1;      
+  stomach_previousButton_State = stomach_buttonState;
+  leftHand_previousButton_State = leftHand_buttonState;
+  rightHand_previousButton_State = rightHand_buttonState;
+
+  delay(mainDelay);
+}
+
+//********************************************************************//
+//********************************************************************//
+//********************************************************************//
+//********************************************************************//
+
+void clicked_left_hand(){
+  Serial.println("Pressed left hand button");
+  startTime = millis();
+  mainDelay = 100;
+  if (currentLibraryIndex == 0){
+    currentLibraryIndex = numOfLibraries-1;      
+  }
+  else{
+    currentLibraryIndex--;
+  }
+  Serial.print("currentLibraryIndex ");
+  Serial.println(currentLibraryIndex);
+  playRandomSong(currentLibraryIndex);
+  delay(10);
+}
+
+void clicked_right_hand(){
+  Serial.println("Pressed right hand button");
+  startTime = millis();
+  mainDelay = 100;
+  if (currentLibraryIndex == numOfLibraries-1){
+    currentLibraryIndex = 0;      
+  }
+  else{
+    currentLibraryIndex++;
+  }
+  Serial.print("currentLibraryIndex ");
+  Serial.println(currentLibraryIndex);
+  playRandomSong(currentLibraryIndex);
+  delay(10);
+}
+
+void clicked_stomach(){
+  Serial.println("Pressed stomach button");
+  startTime = millis();
+  mainDelay = 100;
+  turn_on_leds();
+  playRandomSong(currentLibraryIndex);
+  delay(10);
+}
+
+
+
+bool wait_for_button_press(int buttonPin, int timeWindow){    
+    int buttonState = 1;
+    int previousButton_State = HIGH;  // for checking the state of a pushButton
+    unsigned long endTime;
+    bool buttonPressed = false;
+
+    endTime = millis() + timeWindow;
+    while ((endTime > millis()) and (!buttonPressed)){
+      buttonState = digitalRead(buttonPin);
+      buttonPressed = (buttonState != previousButton_State) && buttonState == LOW;
+      previousButton_State = buttonState;
     }
-    else{
-      currentLibraryIndex--;
-    }
-    Serial.print("currentLibraryIndex ");
-    Serial.println(currentLibraryIndex);
-    playRandomSong(currentLibraryIndex);
-    delay(50);
+  return buttonPressed;    
   }
 
-  // Button 3 case: move one library 'forward' and play a random song out of it
-  else if ((buttonState_3 != previousButton_3_State) && buttonState_3 == LOW){
-    if (currentLibraryIndex == numOfLibraries-1){
-      currentLibraryIndex = 0;      
+void dimLeds(){
+  brightness = brightness - 2; //brightness_interval
+    if (brightness < 0){
+      brightness = 0;
     }
-    else{
-      currentLibraryIndex++;
-    }
-    Serial.print("currentLibraryIndex ");
-    Serial.println(currentLibraryIndex);
-    playRandomSong(currentLibraryIndex);
-    delay(50);
-  }
-  strip.setPixelColor(0, colors[currentLibraryIndex]);  // Set the first LED (eye) to the current color in the array
-  strip.setPixelColor(1, colors[currentLibraryIndex]);  // Set the second LED (eye) to the current color in the array
-  strip.show();
-  previousButton_1_State = buttonState_1;
-  previousButton_2_State = buttonState_2;
-  previousButton_3_State = buttonState_3;  
-  delay(100);
+    strip.setBrightness(brightness);
+    strip.show();
+}
+
+void turn_on_leds(){
+    brightness = 255;
+    strip.setBrightness(brightness);
+    strip.setPixelColor(0, colors[currentLibraryIndex]);  // Set the first LED (eye) to the current color in the array
+    strip.setPixelColor(1, colors[currentLibraryIndex]);  // Set the second LED (eye) to the current color in the array
+    strip.show();
 }
 
 int generateRandomInt(int catagory_length) {
-  Serial.print("catagory_length: ");
-  Serial.println(catagory_length);
   if (catagory_length == 1){ 
     return 0;
   }
@@ -198,10 +260,6 @@ int generateRandomInt(int catagory_length) {
     randomValue = random(0, catagory_length);
     
   }
-  Serial.print("pre: ");
-  Serial.println(randomInt);
-  Serial.print("new: ");
-  Serial.println(randomValue);
   return randomValue;
 }
 
@@ -210,9 +268,8 @@ void playRandomSong(int8_t libraryIndex)
   libraryLength = librariesLengths[libraryIndex];
   randomInt = generateRandomInt(libraryLength);  
   songId = libraries[libraryIndex][randomInt];
-  Serial.print("songId ");
-  Serial.println(songId);
   uint16_t value = (volume << 8) | songId;
+  Serial.println(songId);
   playWithVolume(value);
 }
 
@@ -259,7 +316,7 @@ void sendBytes(uint8_t nbytes)
 void sendCommand(int8_t command, int16_t dat = 0)
 {
   delay(20);
-  if((command == CMD_PLAY_W_VOL)||(command == CMD_SET_PLAY_MODE)||(command == CMD_PLAY_COMBINE))
+  if((command == CMD_PLAY_W_VOL))
   	return;
   else if(command < 0x10) 
   {
@@ -274,4 +331,57 @@ void sendCommand(int8_t command, int16_t dat = 0)
 	mp3_6bytes(command, dat);
   }
   else return;
+}
+
+
+//############## special leds effects ##############//
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+
+void rainbow(uint8_t wait) {
+  brightness = 255;
+  strip.setBrightness(brightness);
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+
+//############## sleep mode functions ##############//
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+
+void Going_To_Sleep(){
+    sleep_enable();//Enabling sleep mode
+    attachInterrupt(0, wakeUp, LOW);//attaching a interrupt to pin d2
+    attachInterrupt(1, wakeUp, LOW);//attaching a interrupt to pin d2
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);//Setting the sleep mode, in our case full sleep
+    sleep_cpu();//activating sleep mode
+    Serial.println("just woke up!");//next line of code executed after the interrupt 
+  }
+
+void wakeUp(){
+  Serial.println("Interrrupt Fired");//Print message to serial monitor
+  sleep_disable();//Disable sleep mode
+  detachInterrupt(0); //Removes the interrupt from pin 2;
+  detachInterrupt(1); //Removes the interrupt from pin 2;
 }
